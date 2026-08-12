f084c4d008439d5b1c7cbb4cb97330e56d86b7c3
Putluru Vijitha <vijithaputluru2003@gmail.com>
2026-08-12 15:18:57 +0530

    Update payslip and attendance changes

--- Frontend/src/TicketManagement/MyTicketsPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import AllTicketsPage from "./AllTicketsPage";
-
-function MyTicketsPage() {
-  return <AllTicketsPage scope="employee" />;
-}
-
-export default MyTicketsPage;
