270fa0f144f6e60eba191cf7567b1edb8b8b290a
PirnavAdmin <awsadmin@pirnav.com>
2026-08-12 10:30:11 +0530

    Fix import path for GlobalToastContainer

--- Frontend/EMS/src/App.jsx
+++ Frontend/EMS/src/App.jsx
@@ -10,7 +10,7 @@ import { AdminPermissionProvider } from "./context/AdminPermissionContext";
 import { EmployeePermissionProvider } from "./context/EmployeePermissionContext";
 import "react-toastify/dist/ReactToastify.css";
 import "./components/common/Toast/toast.css";
-import GlobalToastContainer from "./components/common/Toast/GlobalToastContainer";
+import GlobalToastContainer from "./components/common/toast/GlobalToastContainer";
 import { getStoredToken } from "./utils/authStorage";
 import { hasRole } from "./utils/authorization";
 import {
