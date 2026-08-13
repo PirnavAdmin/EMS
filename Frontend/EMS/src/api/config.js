9c70897adef983cd48b3728f2029412c903fd2b1
Putluru Vijitha <vijithaputluru2003@gmail.com>
2026-08-13 11:29:58 +0530

    Backend Push

--- Frontend/EMS/src/api/config.js
+++ Frontend/EMS/src/api/config.js
@@ -1,11 +1,10 @@
 const API_ORIGIN = (
     import.meta.env.VITE_API_ORIGIN ||
-    "https://cabbage-dramatic-majesty.ngrok-free.dev"
+    // "https://trimestral-flusteredly-patrice.ngrok-free.dev"
+    //  "https://cabbage-dramatic-majesty.ngrok-free.dev"
+    //  "https://marian-undeported-shanon.ngrok-free.dev"
+    "https://hrms.pirnav.com"
 ).replace(/\/+$/, "");
- 
+
 export const SERVER_URL = API_ORIGIN;
 export const BASE_URL = `${API_ORIGIN}/api`;
- 
- 
- 
- 
